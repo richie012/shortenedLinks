3316044024ac4acf87ca5b61b6c708faeaf06ada
richie <82734398+richie012@users.noreply.github.com>
2023-09-26 23:56:28 +0500

    Add files via upload

---
+++ src/main/java/ru/brainybears/shortenedLinks/ShortenedLinksApplication.java
@@ -0,0 +1,13 @@
+package ru.brainybears.shortenedLinks;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShortenedLinksApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ShortenedLinksApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ru/brainybears/shortenedLinks/ShortenedLinksApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.brainybears.shortenedLinks;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ShortenedLinksApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
